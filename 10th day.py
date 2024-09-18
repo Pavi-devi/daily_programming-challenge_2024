7cc610f0ed78fc999047eee58f9efcb3a89ff37f
Pavi-devi <pavithradevi102@gmail.com>
2024-09-18 23:19:39 +0530

    Create 10th day.py

---
+++ 10th day.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    anagram_map = defaultdict(list)
+    
+   
+    for word in strs:
+        
+        sorted_word = ''.join(sorted(word))
+        anagram_map[sorted_word].append(word)
+    
+  
+    return list(anagram_map.values())
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+output = groupAnagrams(strs)
+print(output)
